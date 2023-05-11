9dbbe0985ca2653d41a36b50fb98bbf29b4e4161
majdi <majdi.kaaniche@gmail.com>
2023-05-11 18:57:52 +0100

    add nginx, requirement.txt and entrypoint.sh files

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RUN_PORT="8020"
+
+cd yameer;
+/opt/venv/bin/python manage.py migrate --no-input
+/opt/venv/bin/gunicorn yameer.wsgi --bind "0.0.0.0:${RUN_PORT}" --daemon
+
+
+nginx -g 'daemon off;'
