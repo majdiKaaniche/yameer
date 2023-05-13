ffd0ef13cbd225f1e24e3aef05af9fb48b424f50
majdi <majdi.kaaniche@gmail.com>
2023-05-13 18:47:12 +0100

    add Allowed Host

--- yameer/yameer/settings.py
+++ yameer/yameer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3fy6t0&yk)#ew-%30utz@%rgh9c+u^&$shs45x@d#h@+86ai5&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['main-service-uctwwqju7q-uc.a.run.app']
 
 
 # Application definition
